e6ac837f3c3726060099cbdafebb87f7a27bb516
ahmadammmoura <ammoura575@gmail.com>
2021-07-31 17:18:22 +0300

    done

--- src/controllers/barber/products.controller.js
+++ src/controllers/barber/products.controller.js
@@ -1,6 +1,13 @@
 const createProduct = async (req, res, next) => {
   // create a Product, description and price
 };
+const getProduct = async (req, res, next) => {
+
+  // we need the barber id 
+
+  // get all products for the barbar
+
+};
 
 const editProduct = async (req, res, next) => {
   // update a product
@@ -10,6 +17,7 @@ const deleteProduct = async (req, res, next) => {
   // delete a product
 };
 module.exports = {
+  getProduct,
   createProduct,
   editProduct,
   deleteProduct,
