c345503fc9ebca949ce308724f2a0cdfd5185563
hatem <hatemhtm7@gmail.com>
2021-08-04 04:13:29 +0300

    refactoring functions

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -4,8 +4,6 @@ const jwt = require('jsonwebtoken');
 require('dotenv').config();
 const secret = process.env.SECRET;
 const Interface = require('../Models/auth-model');
-const userB = new Interface('barber');
-const userC = new Interface('client');
 
 const signIn = async (req, res, next) => {
   const user = {
@@ -19,30 +17,15 @@ const signUp = async (req, res, next) => {
   try {
     const pass = await bcrypt.hash(req.body.password, 10);
     req.body.password = pass;
-
     const role = req.body.role;
+    const user = new Interface(`${role}`);
 
-    if (role === 'barber') {
-      const checkUser = await userB.read(req.body.email);
-      const checkClient = await userC.read(req.body.email);
+    const checkUser = await user.read(req.body.email);
 
-      if (checkUser.rows[0]) return next('This email is already a barber registered account');
-      if (checkClient.rows[0]) return next('This email is already a client registered account');
+    if (checkUser.rows[0]) return next(`This email is already a ${role} registered account`);
 
-      const barber = await userB.create(req);
-      res.status(201).json(barber.rows[0]);
-    }
-
-    if (role === 'client') {
-      const checkUser = await userC.read(req.body.email);
-      const checkBerber = await userB.read(req.body.email);
-
-      if (checkUser.rows[0]) return next('This email is already a client registered account');
-      if (checkBerber.rows[0]) return next('This email is already a Barber registered account');
-
-      const client = await userC.create(req);
-      res.status(201).json(client.rows[0]);
-    }
+    const account = await user.create(req);
+    res.status(201).json(account.rows[0]);
   } catch (error) {
     res.status(403).json(error.message);
   }
